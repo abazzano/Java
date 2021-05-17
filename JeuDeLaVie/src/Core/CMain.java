6f7f10deae7cf4a63f0e4fa39947fd848738c27f
abazzano <abazzano@gmail.com>
2021-05-18 00:29:00 +0200

    Add Comment
    Test merge

--- JeuDeLaVie/src/Core/CMain.java
+++ JeuDeLaVie/src/Core/CMain.java
@@ -6,7 +6,7 @@ public class CMain {
 
 	// ENTRY POINT 
 	public static void main(String[] args) {
-				
+		// Frame creation
 		CFrame defaultFrame = new CFrame("Le Jeu de la vie");
 									
 		defaultFrame.setDefaultCloseOperation(CFrame.EXIT_ON_CLOSE);
